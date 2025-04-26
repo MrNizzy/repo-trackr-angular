9044f0dba8f3321138d499b64c3a09e2e18eb1f1
MrNizzy <3134183631y@gmail.com>
2025-04-25 19:17:23 -0500

    Add: Agregar componente de inicio en el routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+    title: 'Inicio - Repos Trackr',
+    pathMatch: 'full',
+  },
+];
